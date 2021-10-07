42603b6fc90f16d91039c1525f8fa1d7a1508e81
HARSHIT <harshitchauhan4036@gmail.com>
2021-10-07 17:10:11 +0530

    initial hello.cpp

---
+++ trial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Namaste World";
+
+    int a , b;
+    cout<<"\nEnter a : ";cin>>a;
+    cout<<"\nEnter b : ";cin>>b;
+
+    cout<<"Sum = "<<a+b;
+}
